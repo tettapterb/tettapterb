a6c136be41aba632c90d351bb7dc1cda9125b0c3
tettapterb <93075700+tettapterb@users.noreply.github.com>
2022-09-01 13:37:53 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "agi5.py",
+    version = "0.1",
+    py_modules = ['agi5'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
